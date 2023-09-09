9b4e19c965504122aa05fac30fb43605c7cd5d23
KhRavshanDev <ravshanxakimov2002@gmail.com>
2023-09-09 16:23:01 +0500

    Reviews' methods

--- src/reviews/reviews.controller.ts
+++ src/reviews/reviews.controller.ts
@@ -6,28 +6,43 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
 } from '@nestjs/common';
 import { ReviewsService } from './reviews.service';
 import { CreateReviewDto } from './dto/create-review.dto';
 import { UpdateReviewDto } from './dto/update-review.dto';
+import { AuthenticationGuard } from 'src/utility/guards/authentication.guard';
+import { CurrentUser } from 'src/utility/decorators/current-user.decorator';
+import { UserEntity } from 'src/users/entities/user.entity';
+import { ReviewEntity } from './entities/review.entity';
+import { AuthorizeGuard } from 'src/utility/guards/authorization.guard';
+import { Roles } from 'src/utility/common/user-roles.enum';
 
 @Controller('reviews')
 export class ReviewsController {
   constructor(private readonly reviewsService: ReviewsService) {}
-
+  @UseGuards(AuthenticationGuard)
   @Post()
-  create(@Body() createReviewDto: CreateReviewDto) {
-    return this.reviewsService.create(createReviewDto);
+  async create(
+    @Body() createReviewDto: CreateReviewDto,
+    @CurrentUser() currentUser: UserEntity,
+  ): Promise<ReviewEntity> {
+    return await this.reviewsService.create(createReviewDto, currentUser);
+  }
+
+  @Get('/all')
+  async findAll() {
+    return await this.reviewsService.findAll();
   }
 
   @Get()
-  findAll() {
-    return this.reviewsService.findAll();
+  async findAllByProduct(@Body('productId') productId: number) {
+    return await this.reviewsService.findAllByProduct(+productId);
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.reviewsService.findOne(+id);
+  async findOne(@Param('id') id: string): Promise<ReviewEntity> {
+    return await this.reviewsService.findOne(+id);
   }
 
   @Patch(':id')
@@ -35,6 +50,7 @@ export class ReviewsController {
     return this.reviewsService.update(+id, updateReviewDto);
   }
 
+  @UseGuards(AuthenticationGuard, AuthorizeGuard([Roles.ADMIN]))
   @Delete(':id')
   remove(@Param('id') id: string) {
     return this.reviewsService.remove(+id);
